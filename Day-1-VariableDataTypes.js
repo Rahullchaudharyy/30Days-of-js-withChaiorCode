6f608f32f0ba39adcef78f33a77cb0d1b5fa31a6
Rahullchaudharyy <user.rahullchaudharryy@gmail.com>
2024-07-13 13:08:43 +0530

    Day-1-Completed

---
+++ Day-1-VariableDataTypes.js
@@ -0,0 +1,49 @@
+/* Task one */
+
+var number = 21;
+console.log(number);
+//Output = 21
+
+/* Task two */ 
+
+let Charecter = "Rahul,Chai or Js";
+console.log(Charecter);
+//Output = Rahul,Chai or Js
+
+/* Activity two */ 
+
+// Task three
+const Value = true;
+console.log(Value);
+// Output = true
+
+/* Activity three */
+
+var Data1 = [1,2,3,4,5];
+var Data2 = {course:"30 days js"};
+var Data3 = true;
+var Data4 = 21;
+var Data5 = "Rahul";
+console.log(typeof Data1);
+console.log(typeof Data2);
+console.log(typeof Data3);
+console.log(typeof Data4);
+console.log(typeof Data5);
+/* Activity four */
+//task 4 
+
+let Count = 2 ;
+console.log(Count)
+Count = 5;
+console.log(Count)
+
+
+/* Activity four */
+// Task 5 
+const NothingJustConst = 21 ; 
+console.log(NothingJustConst)
+NothingJustConst = 50;
+console.log(NothingJustConst)
+
+/* We can't re-assign the value of the const because of its immutable behaviour and in the let and 
+var we can because they both have mutable bahaviour this is th reason ... */
