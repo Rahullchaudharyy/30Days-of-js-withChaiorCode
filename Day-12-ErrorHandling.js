eb787b655f0d67b66709b9c1d38159e91a90f5ec
Rahullchaudharyy <user.rahullchaudharryy@gmail.com>
2024-07-27 15:48:58 +0530

    Day-12-Complete

--- Day-12-ErrorHandling.js
+++ Day-12-ErrorHandling.js
@@ -1,42 +1,42 @@
 
-// // Task first . 
-// function riskyFunction() {
-//     throw new Error("Something went wrong in riskyFunction!");
-//   }
+// Task first . 
+function riskyFunction() {
+    throw new Error("Something went wrong in riskyFunction!");
+  }
 
-//   try {
-//     riskyFunction();
-//   } catch (error) {
-//     console.error("An error occurred:", error.message);
-//   }
-//   /// Task two
-//   function Devide(numerator,denominator){
-//     if(denominator === 0 ){
-//         throw new Error("Can't divide by zero")
-//     }
-//     return console.log(numerator/denominator) 
-//   }
+  try {
+    riskyFunction();
+  } catch (error) {
+    console.error("An error occurred:", error.message);
+  }
+  /// Task two
+  function Devide(numerator,denominator){
+    if(denominator === 0 ){
+        throw new Error("Can't divide by zero")
+    }
+    return console.log(numerator/denominator) 
+  }
 
-//   try{
-//     const result =  Devide(10,0)
-//   }catch (error){
-//     console.error("Error is:", error.message);
-// }
+  try{
+    const result =  Devide(10,0)
+  }catch (error){
+    console.error("Error is:", error.message);
+}
 
-// // Task Three 
-// function exampleFunction() {
-//     try {
-//       console.log("Inside the try block");
-//       throw new Error("error in the try block");
-//     } catch (error) {
-//       console.log("Inside the catch block");
-//       console.error("Caught an error:", error.message);
-//     } finally {
-//       console.log("Inside the finally block");
-//     }
-//   }
+// Task Three 
+function exampleFunction() {
+    try {
+      console.log("Inside the try block");
+      throw new Error("error in the try block");
+    } catch (error) {
+      console.log("Inside the catch block");
+      console.error("Caught an error:", error.message);
+    } finally {
+      console.log("Inside the finally block");
+    }
+  }
 
-//   exampleFunction();
+  exampleFunction();
 
 // Task four 
 
