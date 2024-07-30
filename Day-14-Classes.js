83a58f5d2af4fc8b33317f57c75a06ebf7d29d29
Rahullchaudharyy <user.rahullchaudharryy@gmail.com>
2024-07-30 21:39:29 +0530

    Class concept is not done somewhere so have to revise till then just skip this question

--- Day-14-Classes.js
+++ Day-14-Classes.js
@@ -1,5 +1,5 @@
 
-// TaskTwo
+// task1 
 
 class person {
     constructor(name, age) {
@@ -10,32 +10,77 @@ class person {
 
     UpdateAge(age) {
         this.PersonAge = age
-        console.log(`Hello ${this.PersonName} your age is ${this.PersonAge}`)
+        console.log(`Hello ${this.PersonName} your updated age is ${this.PersonAge}`)
+    }
+  static  Staticmsg(){
+        return `Hello its static massage for the ${this.PersonName}`
+    }
+
+    set PersonName(LastName){
+        return `Hello ${this.LastName}`
     }
 }
-
+ // Task 2 
 const PersonInfo = new person("Rahul", 18)
 PersonInfo.UpdateAge(2000000000000)
 
 
-
-// Task two 
-
-class Studnt extends person {
-    constructor(name, age, id) {
-        super(name, age)
-        this.StudntId = id
-    }
-    getStudetnID() {
-        return this.StudntId
-
-    }
-}
-
-const Student = new Studnt('Rahul', 18, 20160024432)
-console.log(Student)
+console.log(person.Staticmsg())
 
 
 
 // Task three 
+
+class Studentt extends person {
+
+    static studentCount = 0 ;
+
+    constructor(name, age, id) {
+        super(name, age);
+        this.studentId = id;
+        this.name = name;
+        this.age = age
+         Studentt.studentCount++
+         console.log(`Total number of students is ${Studentt.studentCount}`)
+    }
+
+    getStudentID() {
+        return this.studentId;
+    }
+
+    greet() {
+        return `Hello, my name is ${this.name} and my ID is ${this.studentId}.`;
+    }
+}
+
+const student = new Studentt("rahul", 18, 20160024432);
+const student2 = new Studentt("ankit", 18, 20160024432);
+const student3 = new Studentt("krishna", 18, 20160024432);
+console.log(student.greet());
+
+// task 5 
+
+
+class Person {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.personAge = age;
+        console.log(`Hello ${this.fullName}, your age is ${this.personAge}`);
+    }
+
+    // Getter for full name
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    // Setter for lastName
+    set lastName(newLastName) {
+        this._lastName = newLastName;
+    }
+}
+
+// Task 7
+const Human = new Person('John', 'Doe', 30);
+
 
